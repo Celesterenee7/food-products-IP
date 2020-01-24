e24b6443acd0863fdb86d8bf096f062ebad59a14
Celeste Morton <creneemorton@gmail.com>
2020-01-24 13:56:09 -0800

    add admin to users and set as false for default

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -3,19 +3,18 @@ require 'rails_helper'
 describe "the add a product process" do
   it "adds a new product" do
     visit products_path
-    click_link 'Add New Product'
-    visit new_product_path
+    click_link 'Create new product'
     fill_in 'Name', :with => 'Tofu'
     fill_in 'Cost', :with => 10
     fill_in 'Country of origin', :with => "USA"
-    click_on 'Add New Product'
-    expect(page).to have_content 'product successfully added!'
+    click_on 'Create Product'
+    expect(page).to have_content 'Product successfully added!'
     expect(page).to have_content 'Tofu'
   end
 
-  it "gives an error when no product name is entered" do
+  it "gives an error when no name is entered" do
     visit new_product_path
-    click_on 'submit'
+    click_on 'Create product'
     expect(page).to have_content "Name can't be blank"
   end
 end
---
+++ spec/features/add_review_pages_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe "the add a review process"
+  it "adss a review to a product" do
+    
+end
