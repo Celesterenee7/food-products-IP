a5126e48a142b5b76a612a69306e553165492953
Celeste Morton <creneemorton@gmail.com>
2020-01-24 11:18:41 -0800

    add user controller file

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "You've successfully signed up!"
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      flash[:alert] = "There was a problem signing up."
+      redirect_to '/signup'
+    end
+  end
+  
+  def show
+    if current_user != nil
+     @user = current_user
+     render :show
+    else
+      flash[:alert] = "You must be signed in to view this"
+      redirect_to '/signin'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    render :edit
+  end
+  
+  def patch
+    @user = User.find(params[:id])
+    @user.profile_picture.attach(params[:user][:profile_picture])
+    redirect_to user_path
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :admin, :profile_picture)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,14 @@ Rails.application.routes.draw do
   resources :products do
     resources :reviews
   end
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/account/:id' => 'users#show', :as => :user
+  get '/account/:id/edit' => 'users#edit', :as => :edit_user
+  patch '/account/:id' => 'users#patch'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+  
 end
