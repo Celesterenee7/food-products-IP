f89e3b91071ef574ee6764266266de6e267c0dfb
Celeste Morton <creneemorton@gmail.com>
2020-01-24 12:16:31 -0800

    add additional testing to review and products and add features folder for FE testing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 describe Product do
-    it { should have_many :reviews }
+  it { should have_many :reviews }
+end
 
+describe Product do
     it { should validate_presence_of :name }
     it { should validate_presence_of :cost }
     it { should validate_presence_of :country_of_origin }
+end
+
+describe Product do
     it("titleizes the name of an product") do
-      product = Product.create({name: "kale"})
-      expect(product.name()).to(eq("Kale"))
-    end
+        product = Product.create({name: "kale", cost: 3.99, country_of_origin: "Portugal"})
+        expect(product.name).to eq "Kale"
+end
   
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,5 +2,20 @@ require 'rails_helper'
 
 describe Review do
   it { should belong_to(:product)}
-  # it { should validate_length_of(:rating).is_at_least(1).is_at_most(10) }
+end
+
+describe Review do
+  it { should validate_presence_of :author }
+  it { should validate_presence_of :content_body }
+  it { should validate_presence_of :rating }
+end
+
+describe Review do
+  it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5)}
+  it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(1)}
+end
+
+describe Review do
+    it { should validate_length_of(:content_body).is_at_least(50)}
+    it { should validate_length_of(:content_body).is_at_most(250)}
 end
